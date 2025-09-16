3230f3a324e7ea41cfc33a374785a17eff332710
Arjunn Shetty <arjunnshetty603@gmail.com>
2025-09-16 11:25:24 +0530

    Solutioning for friends by age df

--- friends-by-age-dataframe.py
+++ friends-by-age-dataframe.py
@@ -1,12 +1,25 @@
-from pyspark.sql import functions
 from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions as func
 
-spark = SparkSession.builder.appName("FriendsByAgeDF").getOrCreate()
+spark = SparkSession.builder.appName("FriendsByAge").getOrCreate()
 
-df = spark.read.csv("Data/fakefriends-header.csv", header = True, inferSchema = True)
+lines = spark.read.option("header", "true").option("inferSchema", "true").csv("Data/fakefriends-header.csv")
 
-df_filtered = df.groupBy("age").count().orderBy("age")
+# Select only age and numFriends columns
+friendsByAge = lines.select("age", "friends")
 
-df_filtered.show()
+# From friendsByAge we group by "age" and then compute average
+friendsByAge.groupBy("age").avg("friends").show()
+
+# Sorted
+friendsByAge.groupBy("age").avg("friends").sort("age").show()
+
+# Formatted more nicely
+friendsByAge.groupBy("age").agg(func.round(func.avg("friends"), 2)).sort("age").show()
+
+# With a custom column name
+friendsByAge.groupBy("age").agg(func.round(func.avg("friends"), 2)
+  .alias("friends_avg")).sort("age").show()
 
 spark.stop()

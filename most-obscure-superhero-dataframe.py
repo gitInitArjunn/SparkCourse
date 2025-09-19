60aaaacef9ce1b343ba23af2a4ed540c7b6d6d9c
Arjunn Shetty <arjunnshetty603@gmail.com>
2025-09-19 12:40:34 +0530

    Obscure superheroes update

--- most-obscure-superhero-dataframe.py
+++ most-obscure-superhero-dataframe.py
@@ -18,10 +18,12 @@ connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " 
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
 
-bottom10 = connections.sort(func.col("connections").asc()).limit(10)
+bottom10 = connections.filter(func.col("connections") > 1) \
+                        .sort(func.col("connections").asc()) \
+                        .limit(10)
 
 bottom10WithNames = bottom10.join(names, bottom10.id == names.id, "inner") \
-                      .select("name", "connections") \
-                      .orderBy(func.col("connections").asc())
+                        .select("name", "connections") \
+                        .orderBy(func.col("connections").asc())
 
 bottom10WithNames.show(10, truncate=False)
